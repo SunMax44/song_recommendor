2a7f7783212416fa15aef558b77de45dfd7dd6d8
SunMax44 <maxsonntag@posteo.de>
2024-11-21 15:18:28 +0100

    Update final_recommendor_app.py

--- final_recommendor_app.py
+++ final_recommendor_app.py
@@ -48,7 +48,7 @@ if st.button("Let's go!"):
     #song_artist_input = [item.strip().strip('"').lower() for item in text_input.split(',')]
 
     # Search for the song on Spotify
-    spotify_search = sp.search(q=text_input_input, type='track', limit=10)
+    spotify_search = sp.search(q=text_input, type='track', limit=10)
 
     # Store results in session state
     st.session_state.track_names = [song['name'] for song in spotify_search['tracks']['items']]
